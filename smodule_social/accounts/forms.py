bfc309d0e004114888163b8dcea5317b11a69a10
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-02-05 07:48:53 +0600

    Day 36

---
+++ smodule_social/accounts/forms.py
@@ -0,0 +1,1 @@
+from django.contrib.auth import get_user_model
--- smodule_social/accounts/views.py
+++ smodule_social/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
 # Create your views here.
+
+class SignUp(CreateView):
+    pass
